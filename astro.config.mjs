9dcc196c5d8659c75762bc18059b85d87dca68df
Shravan Sathiyanarayanan <bl.en.u4cse23053@bl.students.amrita.edu>
2025-03-30 09:17:16 +0530

    added clfr and vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,16 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import react from '@astrojs/react';
 
+import vue from '@astrojs/vue';
+
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [react()]
+
+  integrations: [react(), vue()],
+  adapter: cloudflare()
 });
